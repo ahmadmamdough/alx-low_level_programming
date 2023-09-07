bdce54f433329140f6c975eb012349a8c09fd398
ahmadmamdough <76851682+ahmadmamdough@users.noreply.github.com>
2023-09-07 20:08:32 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-/*
- * output using puts
-*/
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
