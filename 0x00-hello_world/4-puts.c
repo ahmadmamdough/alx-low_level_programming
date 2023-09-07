811b90a421338e14dcd5f7f68256deeaf719e46f
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-07 19:43:59 +0300

    hello world of the alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 
 int main(void) 
 {
-	char str[50];
-	strcpy(str, "Programming is like building a multilingual puzzle");
+	char str[50] = "Programming is like building a multilingual puzzle"
 
 	puts(str);
 	
