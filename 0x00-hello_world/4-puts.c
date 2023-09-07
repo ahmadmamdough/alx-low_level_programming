90bd8d7a3f6d46e171af539b5376a4b600a67fb0
ahmadmamdough <76851682+ahmadmamdough@users.noreply.github.com>
2023-09-07 19:46:50 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main(void) 
 {
-	char str[50] = "Programming is like building a multilingual puzzle\0";
+	char str[] = "Programming is like building a multilingual puzzle\0";
 
 	puts(str);
 	
