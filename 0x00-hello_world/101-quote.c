127017dfbb06839787cd61d0c37ebce9c9f990ff
ahmadmamdough <76851682+ahmadmamdough@users.noreply.github.com>
2023-09-08 05:41:38 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,7 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	write(2, str, 59);
+	fprintf(stderr,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
