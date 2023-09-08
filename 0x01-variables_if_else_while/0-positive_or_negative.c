b9a1ad1c1a2e09c98b06d2a361ace5a6e96696df
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-08 15:48:47 +0300

    dummmbbb

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,11 +4,9 @@
 /* more headers goes there */
 
 /*
- * this is the main function to do absloutly nothing but it has to be documented
- *
  * main - Entry point
  *
- * Description: linux is a simple but genius OS
+ * Description: dumbbbbbbbb
  *
  * Return: Always 0 (Success)
  */
@@ -18,11 +16,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0) 
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n < 0) 
+	else if (n < 0)
 		printf("%d is negative\n", n);
-	else 
+	else
 		printf("%d is zero\n", n);
 	return (0);
 }
