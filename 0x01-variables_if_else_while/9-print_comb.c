965bccbc679d553864f0a7d4c96f7cf6a0ae30ed
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-08 17:12:14 +0300

    AAAAAAAAA

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,12 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
+		putchar(i + '0');
 		if (i == 9)
 		{
-			putchar('9');
+			putchar('\n');
 			break;
 		}
-		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
 	}
