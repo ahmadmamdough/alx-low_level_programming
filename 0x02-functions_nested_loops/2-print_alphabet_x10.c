f939c0e1015f5d7f2cc943292c6015922395828d
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-12 16:41:07 +0300

    hot fix

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry poin
+ *
+ * Description: 'combinations of numbers'
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		print_alphabet();
+
+	return ();
+}
