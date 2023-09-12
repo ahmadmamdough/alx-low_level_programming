38ea773af107e538ff3d0df9a9d3f9d82c18eb01
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-12 16:35:12 +0300

    hotfix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * main - Entry point
+ *
+ * Description: 'function implementaion of print_alphaber'
+ *
+ * Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
 	int i;
