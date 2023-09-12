0bd7b3e57bfb0bef4fe1d043f2f2923267ef46b2
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-12 18:15:21 +0300

    sign func

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - function implementaion
+ * @n: the number to be checked
+ * Description: 'check for letter case'
+ *
+ * Return: 1 if it's positvie 0 if it's 0 -1 if it's negative
+ */
+int print_sign(int n)
+{
+	if (int n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
