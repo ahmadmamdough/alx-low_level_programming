a2f52c4799dd339f18780be2f58eacb92f4b68ea
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-12 17:39:25 +0300

    hot fix

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ * islower - Entry point
+ *
+ * Description: 'check for letter case'
+ *
+ * Return: Always 0 (Success)
+ */
+int islower(char c)
+{
+	if (c > 'a' && c < 'z')
+		return (1);
+	else 
+		return (0);
+}
