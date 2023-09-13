ff3f92b8ef18f45d5f92a2ef41f5455d70ab7009
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-13 05:50:23 +0300

    fast af boi"

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'combinations of numbers'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int result = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			result += i;
+	}
+	printf("%d\n", result);
+}
