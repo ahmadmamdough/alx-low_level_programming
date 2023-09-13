f969b0ec89e33fde77135f0df839424f0e70c504
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-13 05:27:08 +0300

    falsdj;f

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - print the 9 times table
+ * Description: 'print the times table in a specific format'
+ * @n: the number table
+ * return: void
+ *
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int tmp;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			tmp = i * j;
+			if (j != 0)
+				_putchar(' ');
+
+			if (tmp > 99)
+				printf("%d", tmp);
+			else if (tmp > 9)
+				printf(" %d", tmp);
+			else
+				printf("   %d", tmp);
+			if (j != n)
+				_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
