8496c43bd1cc19e7a12a56ab97a1dda09fcb1b21
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-14 17:13:55 +0300

    hotfixx

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,8 +15,12 @@ void print_diagonal(int n)
 	{
 		if (n > 1)
 			for (j = 0; j < i; j++)
+			{
 				_putchar(' ');
+			}
 		_putchar('\\');
 		_putchar('\n');
 	}
+	if (n <= 0)
+		_putchar('\n');
 }
