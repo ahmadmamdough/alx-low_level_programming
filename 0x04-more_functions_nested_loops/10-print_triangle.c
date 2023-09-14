3c9b381c00b5ef7f06fe77ab50aefbef40ae3942
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-14 18:09:12 +0300

    pyramid

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - implementaion
+ * Description: 'this prints a triangle of hashes
+ * @size: the height of the triangle;
+ * Return: always void
+ */
+
+void print_triangle(int size)
+{
+	int i, s, hash;
+
+	for (i = 1; i <= size; i++)
+	{
+
+		for (s = 0; s < size - i;s++)
+			_putchar(' ');
+
+		for (hash = 0; hash < i; hash++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+
+	if (size <= 0)
+		_putchar('\n');
+}
