c40c847847b34ec17969d71c05bff02069a08517
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-18 19:57:19 +0300

    AAAAAAAAAAAA

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - print half of string
+ * @s: the first char of the string
+ * Return: always void
+ */
+
+void puts_half(char *s)
+{
+	int size = 0;
+	int i;
+
+	while (*(s + size) != '\0')
+		size++;
+
+	if (size % 2 == 0)
+		size /= 2;
+	else
+		size = (size - 1) / 2;
+
+	for (i = 0; i < size(); i++)
+		puts(*(s + i));
+
+}
