372d8226a05060a5202a1bd89c9fdccd166ecfaa
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-18 18:26:38 +0300

    AAAAAAAAA

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - implementaion
+ * @a: the first value to be swapped
+ * @b: the second value
+ * return:always void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *tmp;
+
+	tmp = a;
+	a = b;
+	b = a;
+}
