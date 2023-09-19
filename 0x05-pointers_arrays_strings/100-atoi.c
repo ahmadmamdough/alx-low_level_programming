d0e38434db2052a4fde5fb61f81c238cf20155ea
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-19 05:57:44 +0300

    aaa

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _atoi - implementaion
+ * @s: the string
+ * Return: int the result
+ */
+int _atoi(char *s)
+{
+	int res = 0;
+	int sign = 1;
+
+	while (*s != '\0')
+	{
+		if (*s >= '0' && <= '9')
+		{
+			res *= 10;
+			res += *s - '0';
+		}
+		else
+		{
+			if (res > 0)
+				break;
+			else if (*s == '-')
+				sign *= -1;
+		}
+		s++;
+	}
+	return (res);
+}
