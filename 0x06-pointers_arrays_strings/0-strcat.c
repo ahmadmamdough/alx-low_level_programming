48d4067b66dfe0cc02c52f3a409a708812c0e069
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-20 17:57:41 +0300

    c pointers, strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - concatinate the src to the dest
+ * @dest: the first string
+ * @src: the second string
+ * Return: the bigger string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *i = dest;
+
+	while (*i != '\0')
+		i++;
+
+	*i = *src;
+
+	src++;
+	i++;
+
+	while (*dest != '\0')
+	{
+		*i = *src;
+		i++;
+		src++;
+	}
+	*i = '\0';
+
+	return (dest);
+}
