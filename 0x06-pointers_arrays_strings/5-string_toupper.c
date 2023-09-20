f087b32b7129c9a1b3ab60a67f61c6e4cb52daa7
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-20 19:44:57 +0300

    c stdlib

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - makes all lower case upper case
+ * @s: the string to change
+ * Return: the result string
+ */
+char *string_toupper(char *s)
+{
+	char *c = s;
+
+	while (*c)
+	{
+		if (*c >= 'a' && *c <= 'z')
+			*c += 32;
+	}
+
+	return (s);
+}
