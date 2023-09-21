67bd57885b856870e84b49574a41ac4f19e11a30
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-21 05:50:56 +0300

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,15 +8,15 @@ char *rot13(char *s)
 {
 	int i = 0;
 	int j;
-	char bf[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char af[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char bf[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char af[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (s[i])
+	while (s[i] != '\0')
 	{
 		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == bf[j])
-				s[j] = af[j];	
+				s[j] = af[j];
 			break;
 		}
 		i++;
