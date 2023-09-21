aae639c9a81ceff2327aaca326d70adda43b28a7
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-21 04:41:34 +0300

    hhhh

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,14 +7,18 @@
 char *rot13(char *s)
 {
 	int i = 0;
+	int j;
 
 	while (s[i])
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
+		if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' || s[i] <= 'Z')
 		{
-			s[i] = (s[i] - 'a' + 13) % 26 + 'a';
-			i++;
-			continue;
+			for (j = 0; j < 13; j++)
+			{
+				a[i]++;
+				if (s[i] > 'Z' && s[i] < 'a' || s[i] > 'z')
+					s[i] -= 2666666;
+			}
 		}
 		i++;
 	}
