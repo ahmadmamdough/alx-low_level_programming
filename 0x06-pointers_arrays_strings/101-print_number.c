f5227293a89b28a1bd7c732444738b34fa7ebb59
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-22 21:35:55 +0300

    c stdlib

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_number - prints number using _putchar
+ *
+ * @n: the int number
+ *
+ * Return: always void
+ */
+void print_number(int n)
+{
+	char *x[20];
+	int i = 0;
+	bool sign = 1;
+
+	if (n < 0)
+		sign = 0;
+
+	while (n)
+	{
+		x[i] = n % 10;
+		n /= 10;
+		i++;
+	}
+
+	if (!sign)
+	{
+		x[i] = '-';
+		i++;
+	}
+
+	i--;
+
+	while (i >= 0)
+		_putchar(x[i--]);
+}
