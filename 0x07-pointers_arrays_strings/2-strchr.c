ee1b72f3cebfaec0cc9c4338dc64fcd813875142
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-25 19:33:59 +0300

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - locates a char in a string
+ *
+ * @s: the string to check
+ * @c: the char to look for
+ *
+ * Return: pointer to char if found NULL otherwise
+ */
+char *_strchr(char *s, char c)
+{
+	char *i;
+
+	for (i = s; i != '\0'; i++)
+	{
+		if (*i == c)
+			return (i);
+	}
+	return (NULL);
+}
