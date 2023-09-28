0c20eb41ef465caf3bfae2c12eb8db29ecfe6ac6
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-09-28 11:25:25 +0300

    first recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - implementaion
+ * @s: pointer to the first char of the string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
