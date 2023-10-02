7a98cccd063e0ed3d57f00cf0ac701ef6e537aac
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-10-03 01:37:07 +0300

    hotfix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - entry point
+ * @argc:  number of command line argument
+ * @argv:  array containing command line
+ * Return: always 0
+ */
 int main(int argc, char *argv[])
 {
-	printf("%s", argv[0]);
-	
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
