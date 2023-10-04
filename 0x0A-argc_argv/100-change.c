1881f7740e5340e91c72104c9a8b5b279b92b590
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-10-04 06:23:09 +0300

    change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc:  number of command line argument
+ * @argv:  array containing command line
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0;
+	int tmp;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	tmp = atoi(argv[1]);
+
+	result += tmp / 25;
+	tmp %= 25;
+
+	result += tmp / 10;
+	tmp %= 10;
+
+	result += tmp / 5;
+	tmp %= 5;
+
+	result += tmp / 2;
+	tmp %= 2;
+
+	result += tmp
+
+	printf("%d\n", result);
+
+
+	return (0);
+}
