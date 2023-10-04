24d7123fb402292b5c06e5ff38b238f2ed899f67
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-10-04 13:59:36 +0300

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - entry point
+ * @size: the size of the array
+ * @c: the char to intialize the array with
+ * Return: pointer to the array 
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *buffer;
+	int i;
+
+	if (size == 0)
+		return (NULL);
+
+	buffer = malloc (size * sizeof(char));
+	
+	if (buffer == NULL)
+	{
+		return (NULL);
+	}
+	
+	for (i = 0; i < size; i++)
+		*(buffer + i) = c;
+
+	return (buffer);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+# this is memory dynamic alocation
