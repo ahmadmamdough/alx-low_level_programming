bb945dfaf38cf2b36d0c1abfb5afec29a84d3ff6
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-10-04 14:03:35 +0300

    hotfix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,7 @@
  * create_array - entry point
  * @size: the size of the array
  * @c: the char to intialize the array with
- * Return: pointer to the array 
+ * Return: pointer to the array
  */
 char *create_array(unsigned int size, char c)
 {
@@ -14,13 +14,13 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	buffer = malloc (size * sizeof(char));
-	
+	buffer = malloc(size * sizeof(char));
+
 	if (buffer == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	for (i = 0; i < size; i++)
 		*(buffer + i) = c;
 
