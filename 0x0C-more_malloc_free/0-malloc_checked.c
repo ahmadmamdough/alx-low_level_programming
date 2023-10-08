b184cb0ffbf2755474d2fc121e5406454968ccae
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-10-09 02:03:31 +0300

    first

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - entry point
+ * @b: size
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *buffer;
+
+	buffer = malloc(b);
+
+	if (buffer == NULL)
+		exit(98);
+	
+	return (buffer);
+}
