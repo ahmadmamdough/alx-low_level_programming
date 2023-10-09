033a0d27c2e6c920263f68b76922b92b850f2ca2
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-10-09 10:42:24 +0300

    hotfix

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _realloc - entry point
+ * @ptr: the old buffer
+ * @old_size: the old size
+ * @new_size: reallocation size
+ * Return: the new buffer
+ */
+void *_realloc(void *p, unsigned int old_size, unsigned int new_size)
+{
+	int size = new_size;
+	char *buffer = NULL;
+	int i;
+	char *ptr = p;
+	
+	if (old_size == new_size)
+		return (ptr);
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	buffer = malloc(new_size);
+
+	if (buffer == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (old_size < new_size)
+		size = old_size;
+
+	for (i = 0; i < size; i++)
+		buffer[i] = *(ptr + i);
+
+	return (buffer);
+
+}
