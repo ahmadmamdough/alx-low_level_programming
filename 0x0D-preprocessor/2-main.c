afc57b08a92808893302abbf98b2aebc0f224d42
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-10-11 01:21:27 +0300

    hotfix

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point to print the name of the .c file
+ * Return: always void
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
