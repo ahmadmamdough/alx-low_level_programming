70c62e0023272d331ae8205bd9aa86ea155ffa91
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-10-13 02:58:07 +0300

    hotfix

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - entry point
+ * @n: variable
+ * Return: the sum;
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int sum = 0;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+
+	return (sum);
+
+}
