990aa5a89e7ec924effbd3bab00af58a87e24aa7
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-10-12 04:53:17 +0300

    big project

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - entry point
+ * @argc - number of args
+ * @argv - arguments
+ * Return: always 0;
+ */
+int main(int argc, char *argv[])
+{
+	int a; 
+	int b;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	
+	a = atoi(argv[1]);
+	op = argv[2];
+	b = atoi(argv[3]);
+	
+	if (get_op_func(op) == NULL || op[i] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((op == '/' || op == '%') %% b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op) (a, b));
+
+	return (0);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - addition
+ * @a: the first number
+ * @b: the second number
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - addition
+ * @a: the first number
+ * @b: the second number
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - addition
+ * @a: the first number
+ * @b: the second number
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - addition
+ * @a: the first number
+ * @b: the second number
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - addition
+ * @a: the first number
+ * @b: the second number
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
