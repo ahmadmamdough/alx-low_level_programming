d091f68ee118be54641f09d8f9b222fcfdeeb408
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-10-20 04:46:05 +0300

    hotfix

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * mystartupfunction - enrty point
+ * Return: void
+ */
+void __attribute__ ((constructor)) mystartupfunction(void)
+{
+
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
