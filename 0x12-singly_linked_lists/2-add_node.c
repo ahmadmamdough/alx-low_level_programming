86856fd8670d9f10963def78a71a172deb3aeed0
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-10-20 03:52:53 +0300

    hotfix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,12 +8,12 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t tmp = malloc(sizeof(list_t));
+	list_t *tmp = malloc(sizeof(list_t));
 	if (tmp == NULL)
 		return (NULL);
 	tmp->str = strdup(str);
 	tmp->len = strlen(str);
-	tmp->next = head;
+	tmp->next = *head;
 
 	return (tmp);
 }
