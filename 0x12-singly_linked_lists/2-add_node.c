8a6dcc39682e2f1b0c840d7c0604b3532f4a6aee
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-10-20 03:50:38 +0300

    hotfix

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * add_node - adds node in the beggining
+ * @head: the linked list
+ * @str: the new val fot the node
+ * Return: pointer to the new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t tmp = malloc(sizeof(list_t));
+	if (tmp == NULL)
+		return (NULL);
+	tmp->str = strdup(str);
+	tmp->len = strlen(str);
+	tmp->next = head;
+
+	return (tmp);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,7 @@
 #define _LISTS_
 
 #include <stdlib.h>
-
+#include <string.h>
 
 /**
  * struct list_s - is ls node
@@ -21,4 +21,6 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+
 #endif
