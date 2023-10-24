31945d0df93f58dd1d8404a00b65ede1cca352a4
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-10-25 02:34:41 +0300

    hotfix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,17 @@
 #include "lists.h"
 
 /**
- * free_listint - entry
+ * free_listint2 - entry
  * @head: first element in list
  */
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	if (!head)
 		return;
 
 	if (head->next)
-		free_listint(head->next);
+		free_listint2(head->next);
 	free(head);
+	
+	*head = NULL;
 }
