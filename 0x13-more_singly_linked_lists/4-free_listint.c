2f047ef4d8c973973b8bfa914c1c0bdd0f6da5c1
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-10-25 02:27:31 +0300

    free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * free_listint - entry
+ * @head: first element in list
+ */
+void free_listint(listint_t *head)
+{
+	if (*head->next)
+		free_listint(head->next);
+	free(head);
+}
