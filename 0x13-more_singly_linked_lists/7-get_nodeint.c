e3edea94bf049ae68c70f4a8a091e57007190653
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-10-25 03:15:54 +0300

    access

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - access
+ * @head: the list
+ * @index: the index
+ * Return: pointer to the item
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	int i;
+
+	for (i = 0; i < index; i++)
+	{
+		if (!head->next)
+			return (NULL);
+
+		head = head->next;
+	}
+	return (head);
+}
+
